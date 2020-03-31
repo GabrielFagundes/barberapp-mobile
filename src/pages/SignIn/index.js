8ad2183e70c44041c4516a1135d6e10ebf9f459a
GabrielFagundes <aluzgabriel@hotmail.com>
2020-03-31 17:56:56 -0300

    Initial commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return <Background />;
+}
