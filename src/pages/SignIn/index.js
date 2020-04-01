4a58195763ce345e60c20e7e45c516e08271cec7
GabrielFagundes <aluzgabriel@hotmail.com>
2020-04-01 16:58:06 -0300

    First components - input and button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Background />;
+  return (
+    <Background>
+      <Text>Sign In</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
