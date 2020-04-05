ae70ee2cedd6b652731dbdd8f975186f99b3396d
GabrielFagundes <aluzgabriel@hotmail.com>
2020-04-05 00:56:53 -0300

    Add DateTimePicker to the SelectDateTime page

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -8,7 +8,7 @@ import Background from '~/components/Background';
 
 import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
-export default function SelectProvider() {
+export default function SelectProvider({ navigation }) {
   const [providers, setProviders] = useState([]);
 
   useEffect(() => {
@@ -28,7 +28,11 @@ export default function SelectProvider() {
           data={providers}
           keyExtractor={(provider) => String(provider.id)}
           renderItem={({ item: provider }) => (
-            <Provider>
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
               <Avatar
                 source={{
                   uri: provider.avatar
